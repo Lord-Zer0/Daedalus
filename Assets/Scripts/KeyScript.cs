87d4efc3e9e17aa8118301d485dab3b017fef433
Connor McCauley <cdmc13@outlook.com>
2018-11-27 13:05:35 -0500

    Existing Unity project commit

---
+++ Assets/Scripts/KeyScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyScript : MonoBehaviour {
+	public float speed = 10f;
+
+	private Material m_material;
+
+	private string m_keyID;
+
+	void Start () {
+		// Fetch the material from the Renderer of the GameObject
+		m_material = GetComponent<Renderer>().material;
+		m_keyID = "Key:" + m_material.name;
+		print(m_keyID);
+	}
+
+	void Update () {
+		// Simple movement to help show the importance of the keys
+		transform.Rotate(Vector3.up, speed * Time.deltaTime);
+	}
+
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	private bool[] m_keychain;
+	
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.CompareTag("Key")) {
+			// When a key is reached, determine which one it is by the type
+			other.gameObject.SetActive(false);
+			
+		} else if (other.gameObject.CompareTag("Gate Trigger")) {
+
+
+		}else if (other.gameObject.CompareTag("Goal")) {
+
+		}
+	}
+}
